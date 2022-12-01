7107ccf12f2303148f58bc9d303214a61a2fb109
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2022-12-01 16:45:19 +0400

    add basket create,controler

--- products/logic.py
+++ products/logic.py
@@ -1,4 +1,9 @@
-from products.models import ProductCategory, Product
+from django.http import HttpResponseRedirect
+
+from products.models import ProductCategory, Product, Basket
+
+from django.core.paginator import Paginator
+
 
 def index_page_context():
     context = {
@@ -7,11 +12,10 @@ def index_page_context():
     return context
 
 
-def products_page_context():
+def products_page_context(category_id):
     context = {
         'title': 'Store - Catalog',
         'categories': get_all_categories(),
-        'products': get_all_products(),
     }
     return context
 
@@ -22,3 +26,16 @@ def get_all_categories():
 
 def get_all_products():
     return Product.objects.all()
+
+
+def create_basket_or_plus(request, product_id, current_page):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(current_page)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(current_page)
--- products/views.py
+++ products/views.py
@@ -11,3 +11,16 @@ def index(request):
 def products(request):
     context = products_page_context()
     return render(request, 'products/products.html', context)
+
+
+@login_required
+def basket_add(request, product_id=None):
+    current_page = request.META.get('HTTP_REFERER')
+    create_basket_or_plus(request, product_id, current_page)
+
+
+@login_required
+def basket_delete(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
