395a8fee9c18fcb50476723d402f339a9d6d79f4
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2022-12-04 11:58:57 +0400

    upgrade products admin: Basket admin,Product admin and create Basket admin Inline

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 from products.models import ProductCategory, Product, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
-admin.site.register(Basket)
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category')
+    readonly_fields = ('short_description',)
+    search_fields = ('name',)
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity')
+    readonly_fields = ('created_timestamp',)
+    search_fields = ('user',)
+
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('product', 'created_timestamp')
+    extra = 0
