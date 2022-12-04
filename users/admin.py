cb5de082b1a8153dfd1d05d4c59b570684cc91d3
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2022-12-04 12:44:06 +0400

    upgrade Users admin: Admin resgister and Basket inline in Users admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from users.models import User
+from products.admin import BasketAdminInline
 
-admin.site.register(User)
+# admin.site.register(User)
+
+
+@admin.register(User)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username')
+    inlines = (BasketAdminInline,)
+
